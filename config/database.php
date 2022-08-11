52ece9f9e9477a672df998fd75831e7be8e0602f
Marc Antony Martínez Mejía <marc.antony.martinez.mejia@gmail.com>
2022-08-11 14:53:48 -0500

    db

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-34-193-44-192.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd9kios2cp2rknq'),
+            'username' => env('DB_USERNAME', 'cujgamyqmaihnb'),
+            'password' => env('DB_PASSWORD', '630e72a2c2c8811961bfc764f5c288e1c0c9ed0e0bcf7dccdf54ece3d7bb2c57'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
